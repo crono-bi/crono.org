15b0f83d93a1d7de2b93b58ec51143633542aa3d
Pau <pau.urquizu@crono.net>
2022-11-20 22:34:39 +0100

    Primera personalización

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,31 +1,15 @@
 const { description } = require('../../package')
 
-module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Vuepress Docs Boilerplate',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+const sidebar =require ('./data/sidebar.json')
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
+module.exports = {
+  title: 'Crono SQL',
+  description: description,
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#0000FF' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
     repo: '',
     editLinks: false,
@@ -34,35 +18,16 @@ module.exports = {
     lastUpdated: false,
     nav: [
       {
-        text: 'Guide',
+        text: 'Introducción',
         link: '/guide/',
       },
       {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
+        text: 'Funciones',
+        link: '/functions/',
       }
     ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
-    }
+    sidebar 
   },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
---
+++ src/.vuepress/templates/functions.md
@@ -0,0 +1,14 @@
+---
+template: "API"
+Name: "Funciones CronoSQL"
+Catalog: "D:\GitHub\cronosql.io\cronosql.metadata"
+Api: "functions"
+UrlColumn: "FunctionName"
+Target: "/functions/"
+DirectoryStructure: "Files"
+---
+
+# Función  $FunctionName$
+
+Consulte la documentación en el manual de T-SQL
+
