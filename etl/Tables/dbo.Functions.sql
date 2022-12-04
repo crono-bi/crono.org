6585fe0d60f1869a10f92d80a315ffd6893b6b5f
Pau <pau.urquizu@crono.net>
2022-12-05 00:38:55 +0100

    Añadidas funciones en un Excel (con su descripción y grupo)

--- etl/Tables/dbo.Functions.sql
+++ etl/Tables/dbo.Functions.sql
@@ -3,22 +3,20 @@ CREATE OR REPLACE PROCEDURE
 DELETE AND INSERT dbo.Functions(idFunction)
 select  top 1 over (partition by FunctionName order by FunctionType)
 	f.Name FunctionName,
+	functions.Description,
 	concat(FunctionName  , if(FunctionType='Crono',N' ❇️') , if(IsFavorite=YES,N' ❤️ ') , if(Body IS NuLL,' ' + NCHAR(0xD83D) + NCHAR(0xDEA7))) NameEmoji,
 --	concat(FunctionName  , if(FunctionType='Crono',N' ❇️') , if(IsFavorite=YES,N' ❤️ ') , if(Body IS NuLL,N' ⛔' )) NameEmoji,
-	coalesce(groups.FunctionGroup,'Miscelánea') FunctionGroup,
-	if(FunctionName in ('abs','addition', 'coalesce', 'sum', 'getdate','currentusername', 'concat', 'left','replace','round' )) IsFavorite,
+	concat('index-',coalesce(lower(functions.FunctionGroup),'misc'),'-functions') FunctionGroup,
+	coalesce(functions.IsFavorite,NO) IsFavorite,
 	f.FunctionType, 
-	coalesce(issues.BodyMarkdown,trim(msdn.spanish)) Body
+	CASE 
+		WHEN issues.BodyMarkdown IS NOT NULL THEN issues.BodyMarkdown
+		WHEN nullifempty(functions.Description) IS NOT NULL THEN $'{functions.Description}¬ ¬ ## Comentarios ¬ ¬ `{f.Name}` es una función de SQL estándar. Consulte la documentación completa de la función [`{f.Name}`](https://learn.microsoft.com/es-es/sql/t-sql/functions/{f.Name}-transact-sql) para mayor información.'
+	END Body 
+	
 from Crono$Functions f
 left join @@github.Issues filter (repositoryname='cronosql.io') using (name title)
-left join (
-	select c1 Name, $'{c2}¬ ¬ ## Comentarios ¬ ¬ `{c1}` es una función de SQL estándar. Consulte la documentación completa de la función [`{c1}`](https://learn.microsoft.com/es-es/sql/t-sql/functions/{c1}-transact-sql) para mayor información.' spanish
-	from FILE 'D:\GitHub\cronosql.io\etl\CSV\functions-es.csv'
-) msdn using Name
-left join (
-	select c2 Name, c1 FunctionGroup
-	from FILE 'D:\GitHub\cronosql.io\etl\CSV\grupos.csv'
-) groups using Name
+left join excel.functions using (name functionName)
 where 
  f.name not starts with 'Crono$'
 
---
+++ src/functions/index-conversion-functions.md
@@ -0,0 +1,10 @@
+﻿---
+title: "Funciones de conversión y lógicas"
+Position: 2
+---
+
+
+# Funciones de conversión y lógicas
+
+<section-index />
+
