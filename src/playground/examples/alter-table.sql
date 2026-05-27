50f813c546eb189b43f2a9e657446f7a67d87f01
Pau Urquizu <pau.urquizu@crono.net>
2026-05-27 10:48:10 +0200

    Eliminados ficheros obsoletos

--- src/playground/examples/alter-table.sql
+++
@@ -1,8 +0,0 @@
-/*
-  ALTER TABLE supports standard syntax for adding
-  columns to an existing table.
-*/
-
-ALTER TABLE dwh.DimProducts
-  ADD ProductModel nvarchar(100),
-  ADD Color nvarchar(50)
